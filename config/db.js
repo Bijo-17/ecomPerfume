931f33e7ad6953f3185f0809cb11059d6a1436b9
Bijo P A <bijopa17@gmail.com>
2025-04-26 10:11:24 +0530

    project install first step

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
+
+const connectDB = async ()=>{
+ try {
+
+    await mongoose.connect(process.env.MONGODB_URI)
+     console.log("mongodb connected")
+    
+} catch (error) {
+    console.log("failed to connect mongodb",error.message)
+    process.exit(1)
+}
+}
+
+module.exports = connectDB
